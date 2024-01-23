7080ab37fc87b3fcf0c3d2357d70fb6da5889300
pankazillion <kppankaj1843@gmail.com>
2024-01-24 01:59:25 +0530

    Almost Completed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// import DigitButton from "./components/DigitButton/DigitButton";
+import Calculator from "./components/Calculator/Calculator";
+// import Display from "./components/Display/Display";
+
+function App() {
+  return (
+    <div className="calculator-container">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,12 @@
+import Display from "../Display/Display";
+import "./Calculator.css";
+
+const Calculator = () => {
+  return (
+    <div className="calculator-">
+      <Display />
+    </div>
+  );
+};
+
+export default Calculator;
